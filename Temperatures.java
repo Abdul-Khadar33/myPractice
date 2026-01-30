4ae771f475835e04b28255ac13aa38d677bfd52f
Abdul-Khadar33 <vtu30308@veltech.edu.in>
2026-01-30 22:00:31 +0530

    Create Temperatures.java

---
+++ Temperatures.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[n];
+
+        for(int i = n -1; i >= 0; i--)
+        {
+            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()])
+            {
+                stack.pop();
+            }
+            if(!stack.isEmpty())
+            {
+                ans[i] = stack.peek() - i;
+            }
+            else
+            {
+                ans[i] = 0;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
