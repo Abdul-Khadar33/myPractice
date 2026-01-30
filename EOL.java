e7fe270cb1fac870190ad0ab690b25e3485e7fb9
Abdul-Khadar33 <vtu30308@veltech.edu.in>
2026-01-30 22:15:51 +0530

    Create EOL.java

---
+++ EOL.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        while(sc.hasNext())
+        {
+            String ab = sc.nextLine();
+            System.out.println(n + " " + ab);
+            n++;
+        }
+        sc.close();
+    }
+}
