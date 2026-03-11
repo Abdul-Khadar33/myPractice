a866e8f8305d7f047d4b6db35c11503ca76be4a7
Abdul-Khadar33 <vtu30308@veltech.edu.in>
2026-03-11 23:35:50 +0530

    Create LowestCommonAncestor.java

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class TreeNode
+{
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val)
+    {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+
+public class LowestCommonAncestor
+{
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if(root == null || root == p || root == q)
+        {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left == null)
+        {
+            return right;
+        }
+
+        if(right == null)
+        {
+            return left;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args)
+    {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+
+        TreeNode p = root.left;   
+        TreeNode q = root.right;  
+
+        TreeNode result = lowestCommonAncestor(root, p, q);
+
+        System.out.println("Lowest Common Ancestor: " + result.val);
+    }
+}
